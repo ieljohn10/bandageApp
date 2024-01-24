b74e3f8bed1cf383e61885b8e3cad5865aaa4008
yeyel <arielglvz@gmail.com>
2024-01-25 03:35:34 +0800

    develop the featured products UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,59 @@
-import { Box, Container } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import GridItem from "./components/GridItem/GridItem";
 import {
   CardCover5,
   CardCover6,
   CardCover7,
   CardCover8,
+  ProductCover5,
+  CarbonBook,
+  Book,
+  Arrow,
 } from "../assets/images";
+import Image from "next/image";
+
+const Title = (props: any) => {
+  const { title, weight = 700, fontSize = 24, lineHeight = 32 } = props;
+  return (
+    <Typography
+      {...props}
+      fontFamily="Montserrat"
+      textAlign="center"
+      textTransform="uppercase"
+      lineHeight={`${lineHeight}px`}
+      color={"#252B42"}
+      fontWeight={weight}
+      fontSize={`${fontSize}px`}
+    >
+      {title}
+    </Typography>
+  );
+};
+
+const SubTitle = (props: any) => {
+  const { title, weight = 400, fontSize = 20, lineHeight = 30 } = props;
+  return (
+    <Typography
+      {...props}
+      fontFamily="Montserrat"
+      textAlign="center"
+      textTransform="capitalize"
+      lineHeight={`${lineHeight}px`}
+      color={"#737373"}
+      fontWeight={weight}
+      fontSize={fontSize}
+    >
+      {title}
+    </Typography>
+  );
+};
 
 export default function Home() {
   return (
     <Container>
+      {/* Grid Items */}
       <Box
-        marginY="80px"
+        paddingY="80px"
         sx={{
           display: "grid",
           gridAutoFlow: "row",
@@ -44,6 +86,118 @@ export default function Home() {
           image={CardCover7}
           name={"cardcover5"}
         />
+      </Box>
+
+      {/* Best Seller Products */}
+      <Box paddingY="80px">
+        <Box>
+          <SubTitle title="Featured Products" />
+          <Title title="BESTSELLER PRODUCTS" marginY="10px" />
+          <SubTitle
+            title="Problems trying to resolve the conflict between Products"
+            fontSize={14}
+            lineHeight={20}
+          />
+        </Box>
+
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          gap="24px"
+          marginY="24px"
+          paddingY="24px"
+        >
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 1, sm: 12, md: 20 }}
+          >
+            {Array.from(Array(20)).map((_, index) => (
+              <Grid item xs={1} sm={4} md={4} key={index}>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  flexDirection="column"
+                  width="183px"
+                >
+                  <Image
+                    style={{
+                      objectFit: "cover",
+                      width: "100%",
+                      height: "238px",
+                    }}
+                    src={ProductCover5}
+                    alt={"product"}
+                    loading="lazy"
+                  />
+                  <Box paddingTop="25px" marginBottom="35px">
+                    <Title
+                      title="Graphic Design"
+                      fontSize={16}
+                      lineHeight={24}
+                    />
+                    <SubTitle
+                      title="English Department"
+                      fontSize={14}
+                      lineHeight={24}
+                    />
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      gap={1}
+                    >
+                      <Typography
+                        color="#BDBDBD"
+                        fontSize="16px"
+                        fontWeight={700}
+                        lineHeight="24px"
+                      >
+                        $16.48
+                      </Typography>
+                      <Typography
+                        color="#23856D"
+                        fontSize="16px"
+                        fontWeight={700}
+                        lineHeight="24px"
+                      >
+                        $6.48
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+          <Button
+            variant="outlined"
+            sx={{
+              fontSize: "14px",
+              fontWeight: 700,
+              paddingX: "40px",
+              paddingY: "15px",
+            }}
+          >
+            LOAD MORE PRODUCTS
+          </Button>
+        </Box>
+      </Box>
+
+      {/* Featured Products */}
+      <Box paddingY="80px">
+        <Box>
+          <SubTitle title="Featured Products" />
+          <Title title="THE BEST SERVICES" marginY="10px" />
+          <SubTitle
+            title="Problems trying to resolve the conflict between"
+            fontSize={14}
+            lineHeight={20}
+          />
+        </Box>
+        <Box>
+          <Box paddingX="40px" paddingY="35px"></Box>
+        </Box>
       </Box>
     </Container>
   );
--- assets/images/index.js
+++ assets/images/index.js
@@ -3,4 +3,19 @@ import CardCover6 from "./card-cover-6.jpg";
 import CardCover7 from "./card-cover-7.jpg";
 import CardCover8 from "./card-cover-8.jpg";
 
-export { CardCover5, CardCover6, CardCover7, CardCover8 };
+import ProductCover5 from "./product-cover-5.png";
+
+import Book from "./bx_bxs-book-reader.png";
+import CarbonBook from "./carbon_book.png";
+import Arrow from "./uil_arrow-growth.png";
+
+export {
+  CardCover5,
+  CardCover6,
+  CardCover7,
+  CardCover8,
+  ProductCover5,
+  Book,
+  CarbonBook,
+  Arrow,
+};
